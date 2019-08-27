2ab376752a3cb614a969b9e9952d6e4ee36b414f
vikaschouhan8 <aboutvikaschouhan@gmai.com>
2019-08-27 20:34:39 +0530

    add active class in router

--- src/components/navContent/docs/Docs.js
+++ src/components/navContent/docs/Docs.js
@@ -1,28 +1,72 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
+// //routing
+// import {Route, Link } from "react-router-dom"
 
-export default class Docs extends Component {
-    render() {
-        return (
-            <>
-               <div class="row">
-                    <div class="col-md-8">
+// // const Doc = ({match}) => <h1>I am a docs with id: {match.params.id}</h1>
+// const User = ({ match }) => <p>{match.params.id}</p>
 
-                        <h1>Indore Badi si heading</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, eius. 
-                            Nesciunt sunt maiores, provident officia quisquam ducimus laborum ad dolore 
-                            reiciendis magni ut a adipisci dicta molestias corrupti enim? Ratione.</p>
-                    </div>
-                    <div class="col-md-4">
-                        <ul>
-                        <li>home</li>
-                        <li>from</li>
-                        <li>cgrome</li>
-                        <li>fddf</li>
-                        <li>xfgfhh</li>
-                        </ul>
-                    </div>
-                </div> 
-            </>
-        )
-    }
+
+// export default class Docs extends Component {
+//     render() {
+//         // const { params } = this.props.match
+//         const { url } = this.props.match
+//         // console.log(this.props)
+//         return (
+//             <>
+//                <div className="row">
+//                     <div className="col-md-8">
+
+//                         <h1>Indore Badi si heading</h1>
+//                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, eius. 
+//                             Nesciunt sunt maiores, provident officia quisquam ducimus laborum ad dolore 
+//                             reiciendis magni ut a adipisci dicta molestias corrupti enim? Ratione.</p>
+//                     </div>
+//                     <div className="col-md-4">
+//                         <ul>
+//                             <li>
+//                                 <Link to="/docs/1">docs 1</Link>
+//                             </li>
+//                             <li>
+//                                 <Link to="/docs/2">docs 2</Link>
+//                             </li>
+//                             <li>
+//                                 <Link to="/docs/3">dosc 3</Link>
+//                             </li>
+//                         </ul>
+//                         <Route path="docs/:id" component={User}/>
+//                     </div>
+//                 </div> 
+//             </>
+//         )
+//     }
+// }
+
+
+import React from 'react'
+import { Route, Link } from 'react-router-dom'
+const User = ({ match }) => <p>{match.params.id} fgfdg</p>
+class Docs extends React.Component {
+  render() {
+    // const { url } = this.props.match
+    return (
+      <div>
+        <h1>Users</h1>
+        <strong>select a user</strong>
+        <ul>
+          <li>
+            <Link to="/docs/1">User 1 </Link>
+          </li>
+          <li>
+            <Link to="/docs/2">User 2 </Link>
+          </li>
+          <li>
+            <Link to="/docs/3">User 3 </Link>
+          </li>
+        </ul>
+
+        <Route path="/docs/:id" component={User} />
+      </div>
+    )
+  }
 }
+export default Docs
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import style from './index.css';
-import App from './App';
+import './index.css';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
 //Router import
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import { Route, NavLink, BrowserRouter as Router, Switch } from 'react-router-dom'
 import Docs from "./components/navContent/docs/Docs"
 import Block from "./components/block/Block"
 import Main from './components/main/Main';
 import FooterSection from './components/footerSection/FooterSection';
+import NotFound from './components/notFound/NotFound';
 
 
 const divStyle = {
@@ -18,34 +19,38 @@ const divStyle = {
 
 const routing = (
     <Router>
-        <div className={style.bgColor}>
+        <div className="bgColor">
             <nav className="navbar navbar-default">
                 <div className="container-fluid">
                     <div className="navbar-header">
                     <a className="navbar-brand" href="#">
-                        <img src='./logo.svg' alt="react.org" className={style.imgClass} />
-                        <span className={style.textColor}>MicroSoft</span>
+                        <img src='./logo.svg' alt="react.org" className="imgClass" />
+                        <span className="textColor">MicroSoft</span>
                     </a>
                     </div>
 
                     <ul className="nav navbar-nav" style={divStyle}>
-                        <li className={style.navLink}>
-                            <Link to="/">React</Link>
+                        <li className="navLink">
+                            <NavLink activeClassName="active" to="/">React</NavLink>
                         </li>          
-                        <li className={style.navLink}>
-                            <Link to="/docs">Docs</Link>
+                        <li className="navLink">
+                            <NavLink activeClassName="active" to="/docs">Docs</NavLink>
                         </li>
-                        <li className={style.navLink}>
-                            <Link to="/tutorial">Tutorial</Link>
+                        <li className="navLink">
+                            <NavLink activeClassName="active"  to="/tutorial">Tutorial</NavLink>
                         </li>
                     </ul>
                     
                 </div>
             </nav>
-            <Route exactgit path="/" component={Main}/>
-                    <Route path="/docs" component={Docs} />
-                    <Route path="/tutorial" component={FooterSection} />
+            
         </div>
+        <Switch>
+                <Route exact path="/" component={Main}/>
+                <Route path="/docs" component={Docs} />
+                <Route path="/tutorial" component={FooterSection} />
+                <Route component={NotFound}/>
+            </Switch>
     </Router>
 )
 
